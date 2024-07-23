9a7d9a08fec04875057e09b24b558784be322ccb
Vlad Kravchenko <57967177+VladKravchenko05082000@users.noreply.github.com>
2024-07-23 21:26:01 +0300

    rewrite linkedIn social link

--- src/constants/social-media.ts
+++ src/constants/social-media.ts
@@ -4,7 +4,7 @@ export const SOCIAL_MEDIA_LIST = [
   { Icon: TelegramIcon, href: "https://t.me/Petrovichmladshii" },
   {
     Icon: LinkedInIcon,
-    href: "https://www.linkedin.com/in/%D0%B2%D0%BB%D0%B0%D0%B4%D0%B8%D1%81%D0%BB%D0%B0%D0%B2-%D0%BA%D1%80%D0%B0%D0%B2%D1%87%D0%B5%D0%BD%D0%BA%D0%BE/?trk=opento_sprofile_details",
+    href: "https://www.linkedin.com/in/kravchenkovlad/",
   },
   { Icon: GithubIcon, href: "https://github.com/VladKravchenko05082000" },
   { Icon: GmailIcon, href: "mailto:daitevoitipozalysta@gmail.com" },
